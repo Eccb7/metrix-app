293e71c1bcedc2d72b79b07a4c4d116488343ca3
Briton Otieno <ojwangbriton@gmail.com>
2023-08-14 19:27:35 +0300

    Update App.js file

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import Home from './components/Home';
+import DetailsPage from './components/Details';
+import NavBar from './components/NavBar'; // Import the NavBar component
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <div>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/details/:gameId" element={<DetailsPage />} />
+          </Routes>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
